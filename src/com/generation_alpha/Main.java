39c534ecafc125755709bb3ff81f3c96c9c7f09d
Leonard Box <lfbox7@yahoo.com>
2022-05-03 15:51:36 -0500

    more refactoring of objects AB#9 AB#10

--- src/com/generation_alpha/Main.java
+++ src/com/generation_alpha/Main.java
@@ -1,11 +1,16 @@
 package com.generation_alpha;
 
-import com.generation_alpha.characters.Gyro;
-import com.generation_alpha.client.TextParser;
-import com.generation_alpha.locations.*;
+import com.generation_alpha.client.GameBoard;
+
+import java.io.IOException;
 
 class Main {
     public static void main(String[] args) {
-
+        GameBoard gameBoard = new GameBoard("Home");
+        try {
+            System.out.println(gameBoard.getTextParser().promptInput(gameBoard));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
--- src/com/generation_alpha/client/Get.java
+++ src/com/generation_alpha/client/Get.java
@@ -1,34 +1,32 @@
 package com.generation_alpha.client;
 
+import com.generation_alpha.characters.Gyro;
 import com.generation_alpha.items.HealthBoost;
+import com.generation_alpha.items.Item;
 import com.generation_alpha.items.PowerItem;
 import com.generation_alpha.items.StrengthBoost;
+import com.generation_alpha.locations.GamePlay;
+import com.generation_alpha.locations.Location;
+import com.generation_alpha.locations.Structure;
 
 class Get {
-    public static String forGet(String inputLine){
-        String item = "";
-        // Conditions - Hardcoded items based off possible inputs
-        switch (inputLine) {
-            case "strength":
-                StrengthBoost sb = new StrengthBoost();
-                sb.setStrengthBoost(1);
-                item = "Strength";
-                break;
-            case "health":
-                HealthBoost hb = new HealthBoost();
-                hb.setHealthBoost(1);
-                item = "health";
-                break;
-            case "power":
-                PowerItem pow = new PowerItem();
-                pow.setName("Power");
-                item = "Power";
-                break;
-            default:
-                System.out.println("No such item yet");
-                break;
+    public static String forGet(GameBoard gameBoard, String name) {
+        GamePlay gamePlay = gameBoard.getGamePlay();
+        Gyro gyro = gameBoard.getGyro();
+        Structure location = gyro.getLocation();
+
+        if (name.equals("null")) {
+            return "There is no item at this location.";
         }
+        Item item = gamePlay.getItems(name);
         System.out.println(item);
-        return item;
+        if (item instanceof StrengthBoost || item instanceof HealthBoost) {
+            gyro.addItem(item);
+            location.setItem(new StrengthBoost("null"));
+        } else if (item instanceof PowerItem) {
+            gyro.addPower((PowerItem)item);
+            location.setItem(new PowerItem("null"));
+        }
+        return String.format("%s was added to %s's bag.", item.getName(), gyro.getName());
     }
 }
